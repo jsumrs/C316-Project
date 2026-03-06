f21258ccb18e8619f6dfcc839b38eddb4d3d9964
Brandon Hay <bshay333@gmail.com>
2026-03-06 12:54:18 -0800

    Create Base XCode Project
    -Create new iOS App in Xcode

---
+++ MeetingPlanner/MeetingPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeetingPlanner
+//
+//  Created by Brandon Hay on 2026-03-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
