13b029f02aba488d2327a601d9d825cd519481bc
Brandon Hay <bshay333@gmail.com>
2026-03-06 13:47:52 -0800

    Create Default project
    -Create iOS app Project Base

---
+++ MeetingPlanner/MeetingPlanner/MeetingPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingPlannerApp.swift
+//  MeetingPlanner
+//
+//  Created by Brandon Hay on 2026-03-06.
+//
+
+import SwiftUI
+
+@main
+struct MeetingPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
